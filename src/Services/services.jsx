fce1baae80c11983bf7e89fb7b9b0fbec23d0503
Ethanham111 <mtlham45@gmail.com>
2024-09-02 23:55:22 -0400

    SEO Changes

--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import shaking from '../Images/shaking.jpg';
 import casual from '../Images/casual.jpg';
@@ -13,19 +12,21 @@ function ServicesContainer() {
 
     return (
         <>
-        <div className = 'servicesWrapper'>
-            <a id="servicesSection"> </a>
+        <div className='servicesWrapper'>
+            <a id="servicesSection"></a>
             <h2 ref={(el) => elementsRef.current.push(el)} className="hidden" id="serviceHeader">
-                {language === 'EN' ? 'Our Services' : 'Nos Services'}
+                {language === 'EN' ? 'Our Bar Services' : 'Nos Services de Bar'}
             </h2>
             <div className="servicesText">
                 <p ref={(el) => elementsRef.current.push(el)} className="hidden" id="servicesItalics">
-                    {language === 'EN' ? 'Professional Event Staffing and Mobile Bar Services' : 'Services de personnel d\'événement professionnel et de bar mobile'}
+                    {language === 'EN' ? 
+                        'Professional Bar Services for Weddings, Events, and Private Parties in Montreal' : 
+                        'Services de Bar Professionnels pour Mariages, Événements et Fêtes Privées à Montréal'}
                 </p>
                 <p ref={(el) => elementsRef.current.push(el)} className="hidden" id="servicesParagraph">
                     {language === 'EN'
-                        ? '"Bottoms Up" offers the perfect solution for a wide range of events. Whether you\'re hosting a relaxed backyard barbecue or an elegant wedding reception, we provide a versatile and stylish bar setup that caters to your specific event needs. We ensure your event is not just an occasion but a lasting memory.'
-                        : '"Bottoms Up" offre la solution parfaite pour une large gamme d\'événements. Que vous organisiez un barbecue décontracté dans votre jardin ou une réception de mariage élégante, nous proposons une installation de bar polyvalente et élégante qui répond à vos besoins spécifiques. Nous veillons à ce que votre événement ne soit pas seulement une occasion, mais un souvenir durable.'}
+                        ? 'At "Bottoms Up", we provide exceptional bar services tailored to your event in Montreal. Whether you are hosting a wedding, a private party, or a corporate event, our mobile bar services offer the perfect solution. Our experienced staff ensures a seamless and stylish bar experience that will make your event memorable.'
+                        : 'Chez "Bottoms Up", nous offrons des services de bar exceptionnels adaptés à votre événement à Montréal. Que vous organisiez un mariage, une fête privée ou un événement professionnel, nos services de bar mobile offrent la solution parfaite. Notre personnel expérimenté assure une expérience de bar fluide et élégante qui rendra votre événement inoubliable.'}
                 </p>
             </div>
 
@@ -33,27 +34,39 @@ function ServicesContainer() {
                 <div className="servicesDivs" id="formalDiv">
                     <div className="overlay-box">
                         <div ref={(el) => elementsRef.current.push(el)} className="serviceDescription">
-                            {language === 'EN' ? 'Formal Events' : 'Formal Events'}
+                            {language === 'EN' ? 'Formal Events' : 'Événements Formels'}
                         </div>
                     </div>
                     <div className="serviceDescriptionMobile">
-                        <span>{language === 'EN' ? 'Formal Events' : 'Formal Events'}</span>
+                        <span>{language === 'EN' ? 'Formal Events' : 'Événements Formels'}</span>
                     </div>
-                    <img className="serviceImg" src={formal} alt="Indoor venue with made cocktails on display" />
+                    <img 
+                        className="serviceImg" 
+                        src={formal} 
+                        alt="Bar setup for formal events in Montreal" 
+                    />
                 </div>
 
-                <img id="shaking" src={shaking} alt="Shaking image" />
+                <img 
+                    id="shaking" 
+                    src={shaking} 
+                    alt="Bartender shaking cocktails at a party in Montreal" 
+                />
 
                 <div className="servicesDivs" id="casualDiv">
                     <div className="overlay-box">
                         <div ref={(el) => elementsRef.current.push(el)} className="serviceDescription">
-                            {language === 'EN' ? 'Casual Events' : 'Casual Events'}
+                            {language === 'EN' ? 'Casual Events' : 'Événements Décontractés'}
                         </div>
                     </div>
                     <div className="serviceDescriptionMobile">
-                        <span>{language === 'EN' ? 'Casual Events' : 'Casual Events'}</span>
+                        <span>{language === 'EN' ? 'Casual Events' : 'Événements Décontractés'}</span>
                     </div>
-                    <img className="serviceImg" src={casual} alt="Outdoor venue with mojito pitcher on display" />
+                    <img 
+                        className="serviceImg" 
+                        src={casual} 
+                        alt="Casual outdoor bar setup for parties in Montreal" 
+                    />
                 </div>
             </div>
         </div>
